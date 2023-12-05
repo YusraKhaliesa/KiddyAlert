0e8553efb4b02d424a1e2b0157808fd72f4f27a9
Yusra Khaliesah <y.khaliesah89@icloud.com>
2023-12-05 12:32:22 +0800

    Initial Commit

---
+++ KiddyAlert/KiddyAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiddyAlertApp.swift
+//  KiddyAlert
+//
+//  Created by user on 05/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct KiddyAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
